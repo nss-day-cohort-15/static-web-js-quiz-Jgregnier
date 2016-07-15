d5d3e2a30ea03dea4c6ed6d2f44a3a94abaf0867
James Regnier <jgreg071@gmail.com>
2016-07-15 11:45:42 -0500

    First Update

--- quiz.js
+++ quiz.js
@@ -1,17 +1,27 @@
 var button = document.getElementById("button");
 
-button.addEventListener("click", makeTree)
+button.addEventListener("click", makeTree);
+document.addEventListener("keydown", enter);
 
+function enter(e) {
+    if (13 == e.keyCode) {
+     makeTree()
+ }
+}
 
 function makeTree() {
-var height = document.getElementById("height").value;
-var char = document.getElementById("char").value;
+  var height = document.getElementById("height").value;
+  var char = document.getElementById("char").value;
 
-var addCounter = 1;
+  if(height === "" || char === "") {
+    alert("You need to enter values!")
+  }
 
-for (var i = 0; i < height; i += 0){
+  var addCounter = 1;
+
+  for (var i = 0; i < height; i += 0){
     console.log(" ".repeat(height) + char.repeat(addCounter));
     addCounter += 2;
     height-=1;
- }
+  }
 }
