52bbc2e086f5e725373a3da2484aaa6afd955962
James Regnier <jgreg071@gmail.com>
2016-08-02 08:34:57 -0500

    It's not pretty but I got the tree to start accepting an object

--- quiz.js
+++ quiz.js
@@ -1,27 +1,37 @@
 var button = document.getElementById("button");
+var height = document.getElementById("height");
+var char = document.getElementById("char");
 
 button.addEventListener("click", makeTree);
-document.addEventListener("keydown", enter);
+height.addEventListener("keydown", enter);
+char.addEventListener("keydown", enter);
 
+var treeObj = {};
+console.log(treeObj)
 function enter(e) {
     if (13 == e.keyCode) {
-     makeTree()
- }
+      makeTree()
+      console.log(treeObj)
+  }
+}
+
+function newTree () {
+  treeObj.newHeight = document.getElementById("height").value;
+  treeObj.newChar = document.getElementById("char").value;
+  console.log(treeObj)
 }
 
 function makeTree() {
-  var height = document.getElementById("height").value;
-  var char = document.getElementById("char").value;
-
-  if(height === "" || char === "") {
-    alert("You need to enter values!")
+  newTree();
+  if(treeObj.newHeight === "" || treeObj.newChar === "") {
+      alert("You need to enter values!")
   }
 
   var addCounter = 1;
 
-  for (var i = 0; i < height; i += 0){
-    console.log(" ".repeat(height) + char.repeat(addCounter));
+  for (var i = 0; i < treeObj.newHeight; i += 0){
+    console.log(" ".repeat(treeObj.newHeight) + treeObj.newChar.repeat(addCounter));
     addCounter += 2;
-    height-=1;
+    treeObj.newHeight-=1;
   }
 }
